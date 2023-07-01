226b362e1297b17570878143899604c5b2d7e5be
x3xo <x3xo@local>
2023-06-30 21:40:29 -0700

    transparent everforest

--- config/nvim/lua/user/everforest.lua
+++ config/nvim/lua/user/everforest.lua
@@ -1,2 +1,3 @@
--- vim.g.everforest_background = 'hard'
--- vim.cmd.colorscheme "everforest"
+vim.g.everforest_background = 'hard'
+vim.g.everforest_transparent_background = 1
+vim.cmd.colorscheme "everforest"
