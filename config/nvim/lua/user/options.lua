5d1d05d9435ff5573c6be520717ee3b2a79a0300
x3xo <x3xo@local>
2023-06-30 21:49:32 -0700

    formatting format lua files before save

--- config/nvim/lua/user/options.lua
+++ config/nvim/lua/user/options.lua
@@ -1,41 +1,60 @@
 local options = {
-  number            = true, backup                                                                                     = true,
-  backupdir         = vim.fn.stdpath('state') .. "/backup//",
-  directory         = vim.fn.stdpath('state') .. "/swap//",
-  undofile          = true,
-  undodir           = vim.fn.stdpath('state') .. "/undo//",
-  clipboard         = "unnamedplus",
-  wrap              = false,
-  mouse             = "a",
-  mousemodel        = "extend", -- disable right click menu for copy, paste etc.
+  number = true, backup = true,
+  backupdir = vim.fn.stdpath('state') .. "/backup//",
+  directory = vim.fn.stdpath('state') .. "/swap//",
+  undofile = true,
+  undodir = vim.fn.stdpath('state') .. "/undo//",
+  clipboard = "unnamedplus",
+  wrap = false,
+  mouse = "a",
+  mousemodel = "extend", -- disable right click menu for copy, paste etc.
   -- tabs vs spaces
-  shiftwidth        = 2,
-  tabstop           = 2,
-  softtabstop       = 2,
-  expandtab         = true,
+  shiftwidth = 2,
+  tabstop = 2,
+  softtabstop = 2,
+  expandtab = true,
   -- search
-  ignorecase        = true,
-  smartcase         = true,
-  hlsearch          = true,
-  incsearch         = true,
-  pumheight         = 20, -- pop up menu height
-  modelines         = 0, -- Disable reading vim variables & options from special comments within file header or footer
+  ignorecase = true,
+  smartcase = true,
+  hlsearch = true,
+  incsearch = true,
+  pumheight = 20, -- pop up menu height
+  modelines = 0, -- Disable reading vim variables & options from special comments within file header or footer
   --
-  updatetime        = 250,
+  updatetime = 250,
   --shortmess+      = c -- TODO: check what this does
-  signcolumn        = "yes",
-  cursorline        = true,
-  termguicolors     = true,
-  wildmenu          = true,
-  wildmode          = "longest,full",
-  splitbelow        = true,
-  showmode          = false, -- disable showing mode under status line since lualine shows it already. Enable if not using lualine
-  splitright        = true,
+  signcolumn = "yes",
+  cursorline = true,
+  termguicolors = true,
+  wildmenu = true,
+  wildmode = "longest,full",
+  splitbelow = true,
+  showmode = false, -- disable showing mode under status line since lualine shows it already. Enable if not using lualine
+  splitright = true,
+  laststatus = 3,
 }
 
 for k, v in pairs(options) do
   vim.opt[k] = v
 end
+
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = "*.lua",
+  -- command = "normal mpgg=G`pzz",
+  callback = function()
+    vim.lsp.buf.format({
+      formatting_options = {
+        tabSize = 2,
+        insertSpaces = true,
+        -- trimFinalNewlines = false, -- this doesn't seem to work
+        insertFinalNewline = true,
+      },
+    })
+  end
+})
+
+vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
 
 -- vimscript stuff
 
@@ -53,10 +72,10 @@ let $NVIM_TUI_ENABLE_TRUE_COLOR=1
 
 " fix stupid auto commenting
 augroup fixAutoComment
-  autocmd!
-  " set formatoptions-=o "dont continue comments when pushing o/O
-  " autocmd FileType * set formatoptions-=o
-  autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o formatoptions-=t
+autocmd!
+" set formatoptions-=o "dont continue comments when pushing o/O
+" autocmd FileType * set formatoptions-=o
+autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o formatoptions-=t
 augroup END
 
 " don't map Bclose to 'bd
@@ -81,4 +100,3 @@ let g:highlightedyank_highlight_duration = 400
 let g:colorizer_fgcontrast=0
 
 ]])
-
