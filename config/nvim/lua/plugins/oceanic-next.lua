52aef21471e6816efacf9383ebdbac5dc547cc36
x3xo <x3xo@local>
2024-04-22 16:44:56 -0700

    vim colors

--- config/nvim/lua/plugins/oceanic-next.lua
+++ config/nvim/lua/plugins/oceanic-next.lua
@@ -1,14 +1,16 @@
 return {
   'mhartington/oceanic-next',
-  -- config = function()
-  --   vim.cmd.colorscheme "OceanicNext"
-  --   vim.cmd([[
-  --   hi Normal guibg=NONE ctermbg=NONE
-  --   hi LineNr guibg=NONE ctermbg=NONE
-  --   hi SignColumn guibg=NONE ctermbg=NONE
-  --   hi EndOfBuffer guibg=NONE ctermbg=NONE
-  --   hi CursorLine guibg=#1b2b34
-  --   hi! default link CursorLineNr CursorLine
-  --   ]])
-  -- end,
+  config = function()
+    vim.cmd.colorscheme "OceanicNext"
+    vim.cmd([[
+    hi Normal guibg=NONE ctermbg=NONE
+    hi LineNr guibg=NONE ctermbg=NONE
+    hi SignColumn guibg=NONE ctermbg=NONE
+    hi EndOfBuffer guibg=NONE ctermbg=NONE
+    hi CursorLine guibg=#1b2b34
+    hi DiffAdded guifg=#000000
+    hi DiffRemoved guifg=#000000
+    hi! default link CursorLineNr CursorLine
+    ]])
+  end,
 }
--- config/nvim/lua/user/everforest.lua
+++ config/nvim/lua/user/everforest.lua
@@ -1,3 +1,3 @@
-vim.g.everforest_background = 'hard'
+-- vim.g.everforest_background = 'hard'
 -- vim.g.everforest_transparent_background = 1
-vim.cmd.colorscheme "everforest"
+-- vim.cmd.colorscheme "everforest"
