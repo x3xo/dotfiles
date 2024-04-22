376dc3401c8593013edbb8c090d3b3d71431069e
x3xo <x3xo@local>
2024-04-22 11:55:01 -0700

    nvim colors

---
+++ config/nvim/lua/plugins/tokyo-night.lua
@@ -0,0 +1,21 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function()
+    -- require("tokyonight").setup({
+    --   on_highlights = function(hl, c)
+    --     local prompt = "#2d3149"
+    --     hl.Normal = {
+    --       bg = "#000000"}
+    --       hl.TelescopeNormal = {
+    --         bg = c.bg_dark,
+    --         fg = c.fg_dark,
+    --       }
+    --     end,
+    --   })
+
+    -- vim.cmd[[colorscheme tokyonight-night]]
+  end
+}
