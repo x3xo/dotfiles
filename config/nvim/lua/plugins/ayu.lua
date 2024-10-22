74431b2fd7ce389a7c6be7ea5a267773844da60f
x3xo <x3xo@local>
2024-10-22 11:31:18 -0700

    vim tweaks

--- config/nvim/lua/plugins/ayu.lua
+++ config/nvim/lua/plugins/ayu.lua
@@ -2,11 +2,12 @@ return {
   'Shatur/neovim-ayu',
   name = 'neovim-ayu',
   config = function()
-    -- require('ayu').setup({
-    --   mirage = false, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
-    --   overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
-    -- })
+    require('ayu').setup({
+      mirage = false, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
+      overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
+    })
     -- vim.cmd('colorscheme ayu-mirage')
+    -- vim.cmd('colorscheme ayu')
     -- vim.cmd([[
     --   " hi StatusLine guibg=#1b2733
     --   " hi StatusLineNC guibg=#1b2733 guifg=#3e4b59
