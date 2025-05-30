921932ee26db238115c019c7d375132bb24faf2b
x3xo <x3xo@local>
2025-05-30 09:41:11 -0700

    hammerspoon key bindings for resolution switching and menu bar auto hide toggle

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -30,13 +30,13 @@ mydock = require("dock")
 mydock.addItem(mydock.newItemWithKeybinding("k", "Emacs"))
 -- mydock.addItem(mydock.newItemWithKeybinding("l", "Visual Studio Code", true))
 mydock.addItem(mydock.newItemWithKeybinding("v", "VMware Fusion", true))
-mydock.addItem(mydock.newItemWithKeybinding("w", "WezTerm", true))
+-- mydock.addItem(mydock.newItemWithKeybinding("w", "WezTerm", true))
 mydock.addItem(mydock.newItemWithKeybinding(";", "Ghostty", true))
 mydock.addItem(mydock.newItemWithKeybinding("u", "Zen Browser", true))
-mydock.addItem(mydock.newItemWithKeybinding("h", "Vivaldi", true))
+-- mydock.addItem(mydock.newItemWithKeybinding("h", "Vivaldi", true))
 mydock.addItem(mydock.newItemWithKeybinding("i", "Brave Browser", true))
-mydock.addItem(mydock.newItemWithKeybinding("s", "Sublime Text"))
-mydock.addItem(mydock.newItemWithKeybinding("d", "Finder", nil, displayFinder))
+-- mydock.addItem(mydock.newItemWithKeybinding("s", "Sublime Text"))
+-- mydock.addItem(mydock.newItemWithKeybinding("d", "Finder", nil, displayFinder))
 mydock.addItem(mydock.newBrightnessItem())
 -- mydock.addItem(mydock.newNightItem())
 -- mydock.addItem(mydock.newDayItem())
@@ -77,6 +77,25 @@ local setResolutionTo1440p = function ()
 end
 
 setResolutionTo1440p()
+
+local setResolutionTo1080p = function ()
+    local screens = hs.screen.allScreens()
+    for _, screen in ipairs(screens) do
+      screen:setMode(1920, 1080, 2, 60, 8)
+    end
+end
+
+hs.hotkey.bind({ "cmd", "alt", "ctrl", "shift" }, "p", setResolutionTo1440p)
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "p", setResolutionTo1080p)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "b", function()
+    hs.osascript.applescript([[
+        tell application "System Events"
+            tell dock preferences to set autohide menu bar to not autohide menu bar
+        end tell
+    ]])
+end)
+
 
 -- hs.loadSpoon('ControlEscape'):start() -- Load Hammerspoon bits from https://github.com/jasonrudolph/ControlEscape.spoon
 cherry = hs.loadSpoon('Cherry')
